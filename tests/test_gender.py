a1394ffc295708f1cc87711be17f6501d1312d7b
yasin <mj.jaberynasr@gmail.com>
2026-09-14 14:06:05 +0330

    Update and rename test.py to test_gender.py

---
+++ tests/test_gender.py
@@ -0,0 +1,38 @@
+#instal openmts first
+
+from openmts import GenderPredModel
+import cv2
+
+gender_model = GenderPredModel("baseline")
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+# Open webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        face_img = frame[y:y+h, x:x+w]
+        gender = gender_model.predict(face_img) # auto pre-proccesses
+
+        # Draw box
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        cv2.putText(frame, f"{gender}", (x, y-60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
+
+    cv2.imshow("OpenMTS Live Test", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
